1262228575af3cc8cf7a2cc3df41e96010ee3344
yamashirory <74095136+yamashirory@users.noreply.github.com>
2021-01-22 15:24:15 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @prototypes = Prototype.all
+    @prototypes = @user.prototypes
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'prototypes/index'
+  root to: "prototypes#index"
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :users, only: :show
+end
